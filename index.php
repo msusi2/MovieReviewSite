105d447c721126c1d63f06040934c6dc26e82680
bodzee94 <dbodden94@gmail.com>
2017-04-10 14:01:32 -0400

    refined some code

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 	ini_set('display_errors',1);
   error_reporting(E_ALL);
 
@@ -18,8 +17,8 @@
 		$tbl = "tbl_movies";
 		$getMovies = getAll($tbl);
 	}
-
 ?>
+
 <!doctype html>
 <html>
 <head>
